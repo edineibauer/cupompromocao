43f359612cb05d21ef9c54e68c863ec5fb47a178
Edinei J. Bauer <galera.org@gmail.com>
2019-03-21 11:16:33 -0300

    entidades up

--- public/api/produtos/read.php
+++ public/api/produtos/read.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once PATH_HOME . 'public/api/campanhas/read.php';
+include_once PATH_HOME . VENDOR . 'cupompromocao/public/api/campanhas/read.php';
 
 $read = new \Conn\Read();
 $produtos = [];
